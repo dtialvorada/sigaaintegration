64944977059897bbb74f8f7c1a0d6c7cb55d88d9
Vinicius Rosa Seus <vinicius.seus@alvorada.ifrs.edu.br>
2025-01-30 10:38:28 -0300

    ajustado import_categories_adhoc_task nome da chamada da classe

--- classes/task/import_categories_adhoc_task.php
+++ classes/task/import_categories_adhoc_task.php
@@ -3,7 +3,7 @@ namespace local_sigaaintegration\task;
 
 use core\task\adhoc_task;
 use local_sigaaintegration\sigaa_categories_sync;
-use local_sigaaintegration\sigaa_periodo_letivo;
+use local_sigaaintegration\sigaa_academic_period;
 
 class import_categories_adhoc_task extends adhoc_task {
 
@@ -21,7 +21,7 @@ class import_categories_adhoc_task extends adhoc_task {
     }
 
     public function execute() {
-        $period = sigaa_periodo_letivo::getAcademicPeriod();
+        $period = sigaa_academic_period::getAcademicPeriod();
         $categoriessync = new sigaa_categories_sync($period->getAno(), $period->getPeriodo());
         $categoriessync->sync();
     }
